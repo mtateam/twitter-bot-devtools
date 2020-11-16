a7c93bf17eabb25d9a5a1b099361ff863dd4e286
Ali Gümüş Tosun <a_gms_t@hotmail.com>
2020-11-16 11:23:24 +0300

    twitter.js is better now

--- twitter.js
+++ twitter.js
@@ -1,38 +1,55 @@
 let list = [];
 let arrangeInterval = async () => {
-  let followersDiv = document.querySelector(
-    '[aria-label="Timeline: Followers"]'
-  );
-  list = document.querySelector("body").querySelectorAll("span");
-  list = [...list].filter((el) => el.innerText == "Follow");
-  if (list.length == 0) {
-    document.scrollingElement.scrollTop = followersDiv.scrollHeight;
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    arrangeInterval();
-  }
-  let index = 1;
-  console.log(index);
-  console.log(list);
-  let interval = setInterval(async () => {
-    if (index <= list.length - 2) {
-      console.log(index);
-      list[index].scrollIntoView();
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      document.scrollingElement.scrollTop += -150;
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      list[index].innerText == "Follow" && list[index].click();
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      list[index + 1].innerText == "Follow" && list[index + 1].click();
-      index += 2;
-    } else {
+  return new Promise(async (resolve, reject) => {
+    let followersDiv = document.querySelector(
+      '[aria-label="Timeline: Followers"]'
+    );
+    list = document.querySelector("body").querySelectorAll("span");
+    list = [...list].filter((el) => el.innerText == "Follow");
+    if (list.length == 0) {
       document.scrollingElement.scrollTop = followersDiv.scrollHeight;
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      clearInterval(interval);
-      arrangeInterval();
+      resolve(true);
     }
-  }, 20000);
+    let index = 1;
+    console.log(index);
+    console.log(list);
+    let interval = setInterval(async () => {
+      if (index <= list.length - 2) {
+        console.log(index);
+        list[index].scrollIntoView();
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        document.scrollingElement.scrollTop += -150;
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        list[index].innerText == "Follow" && list[index].click();
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        list[index + 1].innerText == "Follow" && list[index + 1].click();
+        index += 2;
+      } else {
+        document.scrollingElement.scrollTop = followersDiv.scrollHeight;
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        clearInterval(interval);
+        resolve(true);
+      }
+    }, 60000);
+  });
 };
-arrangeInterval();
+
+let awaitInterval = async () => {
+  let res = true;
+  let idx = 0;
+  while (res) {
+    try {
+      idx++;
+      res = await arrangeInterval();
+    } catch (error) {
+      console.log({ error });
+      res = false;
+    }
+  }
+};
+
+awaitInterval();
